40569b36c0d7a6adbc7a8854b277e94f6ff84107
Luan de Oliveira <luanmacedo108@gmail.com>
2022-04-11 21:22:50 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production"){
     module.exports = {
-        mongoURI: "mongodb+srv://luanoliveira:<passwordluanç>@cluster0.drdub.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+        mongoURI: "mongodb+srv://luanoliveira:<password>@blogapp.8fj1u.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     }
 }else{
     module.exports = {
