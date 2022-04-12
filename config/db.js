b7e13fc06aeb44141baf5f07710bfb83598c44ce
Luan de Oliveira <luanmacedo108@gmail.com>
2022-04-12 10:01:14 -0300

    Update db.js
    update connection

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV == "production"){
     module.exports = {
-        mongoURI: "mongodb+srv://luanoliveira:<passwordluanç>@blogapp.8fj1u.mongodb.net/blogapp?retryWrites=true&w=majority"
+        mongoURI: "mongodb+srv://luanoliveira:5RNS0C0M1TfyyJx7@bloggap-database.7dpdk.mongodb.net/bloggap-database?retryWrites=true&w=majority"
     }
 }else{
     module.exports = {
